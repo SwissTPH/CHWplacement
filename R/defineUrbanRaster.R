0ef8a0935ad6a07a0dfeb5c1c88052f551837468
clchampag <clara.champagne@swisstph.ch>
2023-02-16 17:11:47 +0100

    add test and documentation for new terra function on urban definition

--- R/defineUrbanRaster.R
+++ R/defineUrbanRaster.R
@@ -28,7 +28,12 @@ DefineUrban=function(population.raster, rururb_cutoff=300, min_urbsize=2000){
   return(urb.raster)
 }
 
-#' Define urban-only pixels.
+#' @title Defines rural and urban areas based on population density, using the terra package
+#' @description This function defines urban areas as those with a population density
+#' higher than rururb_cutoff per pixel. Urban areas are also contiguous and have a minimal
+#' total population equal to min_urbsize. The methodology is derived from \url{http://documents.worldbank.org/curated/en/709121516634280180/pdf/122880-V1-WP-P156561-OUO-9-FINAL-ENGLISH.pdf}.
+#' This function differs from _DefineUrban_ in that the _terra_ package is used (instead of _raster_).
+#' The results between the 2 functions may differ due to different handling of diagonal values in the _terra_ and _raster_ packages.
 #'
 #' @param population_raster An input raster containing people per pixel. Default inputs assume input resolution to be approximately 1 sq. km. resolution.
 #' @param rururb_cutoff Minimum population per pixel to be eligible for urban classification.
@@ -41,7 +46,7 @@ DefineUrban=function(population.raster, rururb_cutoff=300, min_urbsize=2000){
 #' @importFrom scales percent
 #'
 #' @return A SpatRaster object similar to population.raster in which all non-urban pixels are masked (i.e. NA), or a categorized raster where 0 = Rural and 1 = Urban if mask = TRUE.
-#'
+#' @export
 define_urban <- function(population_raster, rururb_cutoff = 300, min_urbsize = 2000, directions = 8, mask = FALSE, verbose = FALSE) {
   # A terra-based implementation of https://github.com/SwissTPH/CHWplacement/blob/main/R/defineUrbanRaster.R
 
--- tests/testthat/test_urban.R
+++ tests/testthat/test_urban.R
@@ -33,3 +33,47 @@ test_that("test urban area definition", {
   names(pop.dummy.3)="pop"
   expect_equal(urb3, pop.dummy.3, label = "test when only 1 panel is urban, due to cutoff and urbsize")
 })
+
+test_that("test urban area definition", {
+  # create a dummy population
+  pop.dummy.low=raster::raster(matrix(c(rep(100,225), rep(5,225)), ncol=30))
+  pop.dummy.up=raster::raster(matrix(c(rep(50,225), rep(200,225)), ncol=30))
+  pop.dummy.up@extent@ymin=1
+  pop.dummy.up@extent@ymax=2
+
+  pop.dummy=raster::merge(pop.dummy.low,pop.dummy.up)
+  names(pop.dummy)="pop"
+  # visual check of the dummy population
+  #rasterVis::gplot(pop.dummy)+ geom_tile(aes(fill=factor(value)), alpha=0.6) + labs(fill="Population")
+
+
+  urb0=DefineUrban(pop.dummy, rururb_cutoff = 3)  # all is urban
+  urb0_terra=define_urban(pop.dummy, rururb_cutoff = 3)  # all is urban
+  expect_true(all.equal(urb0_terra, terra::rast(urb0)), label = "compare terra and raster, all urban")
+
+  urb1=DefineUrban(pop.dummy, rururb_cutoff = 40)  # half if urban, because of the cutoff
+  urb1_terra=define_urban(pop.dummy, rururb_cutoff = 40)  # half if urban, because of the cutoff
+  expect_true(all.equal(urb1_terra, terra::rast(urb1)), label = "compare terra and raster, due to cutoff")
+
+  urb2=DefineUrban(pop.dummy, rururb_cutoff = 40, min_urbsize = 30000)
+  urb2_terra=define_urban(pop.dummy, rururb_cutoff = 40, min_urbsize = 30000)  # half if urban, because of the cutoff
+  expect_true(all.equal(urb2_terra, terra::rast(urb2)), label = "compare terra and raster, due to cutoff and urbsize")
+
+  urb3=DefineUrban(pop.dummy, rururb_cutoff = 60, min_urbsize = 30000)
+  urb3_terra=define_urban(pop.dummy, rururb_cutoff = 60, min_urbsize = 30000)
+  pop.dummy.low3=raster::raster(matrix(rep(NA,450), ncol=30))
+  pop.dummy.up3=raster::raster(matrix(c(rep(NA,225), rep(200,225)), ncol=30))
+  pop.dummy.up3@extent@ymin=1
+  pop.dummy.up3@extent@ymax=2
+  pop.dummy.3=raster::merge(pop.dummy.low3,pop.dummy.up3)
+  names(pop.dummy.3)="pop"
+  expect_equal(urb3, pop.dummy.3, label = "test when only 1 panel is urban, due to cutoff and urbsize")
+
+  # on this specific example, raster and terra provide different results
+  pop.dummy.low3.terra=raster::raster(matrix(c(rep(100,225), rep(NA,225)), ncol=30))
+  pop.dummy.low3.terra@extent@ymin=0
+  pop.dummy.low3.terra@extent@ymax=1
+  pop.dummy.3.terra=raster::merge(pop.dummy.low3.terra,pop.dummy.up3)
+  names(pop.dummy.3.terra)="pop"
+  expect_true(all.equal(urb3_terra, terra::rast(pop.dummy.3.terra)), label = "test when only 1 panel is urban, due to cutoff and urbsize, terra")
+})
